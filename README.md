af742d155af0da6d746a708c7d7cdfe5061cf530
Cris Mihalache <me@f3rno.com>
2020-03-15 14:35:24 +0700

    (initial commit)

---
+++ README.md
@@ -0,0 +1,22 @@
+### Bitfinex Market Prices Blocklet for i3
+
+![screenshot](/readme_assets/screenshot.png)
+
+Sample configuration:
+
+```
+[bitfinex]
+command=/path/to/index.js
+symbol=BTCUSD
+interval=30
+
+[bitfinex]
+command=/path/to/index.js
+symbol=ETHUSD
+interval=30
+
+[bitfinex]
+command=/path/to/index.js
+symbol=LEOUSD
+interval=30
+```
---
+++ index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict'
+
+const { symbol: BLOCK_SYMBOL } = process.env
+const { RESTv2 } = require('bfx-api-node-rest')
+
+const CCY_ICONS = {
+  USD: '$',
+  EUR: '€',
+  JPY: '¥',
+  GBP: '£',
+  BTC: '\uf15a',
+  ETH: '\ufcb9'
+}
+
+const rest = new RESTv2({ transform: true })
+
+const run = async () => {
+  const symbol = BLOCK_SYMBOL[0] === 't' ? BLOCK_SYMBOL : `t${BLOCK_SYMBOL}`
+  const base = symbol.substring(1, 4)
+  const quote = symbol.substring(4)
+  const ticker = await rest.ticker(symbol)
+  const { lastPrice, dailyChangePerc } = ticker
+  let str = `${base} ${lastPrice}`
+
+  if (CCY_ICONS[quote]) {
+    str += ` ${CCY_ICONS[quote]}`
+  }
+
+  str += ` ${(dailyChangePerc * 100).toFixed(2)}%`
+
+  console.log(str)
+}
+
+try {
+  run()
+} catch (e) {
+  console.error(e.message)
+}
